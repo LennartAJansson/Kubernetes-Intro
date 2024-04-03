269bf0af13115dd63c3c21012ebbc37498ce7742
lennartajansson <lennart.a.jansson@outlook.com>
2024-04-03 11:17:02 +0200

    Added some domain actions as a sample, this makes the endpointservice cleaner

--- BuildVersionsApi.Domain/Services/EndpointsService.cs
+++ BuildVersionsApi.Domain/Services/EndpointsService.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using BuildVersionsApi.Domain.Abstract;
+using BuildVersionsApi.Domain.Actions;
 using BuildVersionsApi.Domain.Model;
 using BuildVersionsApi.Domain.Types;
 
@@ -11,24 +12,47 @@ public class EndpointsService(IStorageService service) : IEndpointsService
 {
   public async Task<BuildVersion?> HandleCreateProject(BuildVersion buildVersion, string username, CancellationToken cancellationToken)
   {
-    //HINT Add business logic here: Should handle the created, changed and username
-    buildVersion.Created = DateTime.Now;
-    buildVersion.Changed = DateTime.Now;
-    buildVersion.Username = username;
+    buildVersion = buildVersion.MarkAsChanged(username, true);
+
     return await service.CreateProject(buildVersion, cancellationToken);
   }
 
-  public async Task<BuildVersion?> HandleDelete(string projectName, string username, CancellationToken cancellationToken)
+  public async Task<BuildVersion?> HandleIncreaseVersion(string projectName, VersionNumber version, string username, CancellationToken cancellationToken)
   {
-    //HINT Add business logic here: Using a soft delete on the object and register the changed and username
     BuildVersion? buildVersion = await service.GetByName(projectName, cancellationToken);
     if (buildVersion is null)
     {
       return null;
     }
 
-    buildVersion.Changed = DateTime.Now;
-    buildVersion.Username = username;
+    buildVersion = buildVersion.IncrementVersion(version).MarkAsChanged(username);
+
+    return await service.UpdateProject(buildVersion, cancellationToken);
+  }
+
+  public async Task<BuildVersion?> HandleUpdateProject(BuildVersion newBuildVersion, string username, CancellationToken cancellationToken)
+  {
+    BuildVersion? buildVersion = await service.GetById(newBuildVersion.Id, cancellationToken);
+    if (buildVersion is null)
+    {
+      return null;
+    }
+
+    buildVersion = buildVersion.CloneValuesFrom(newBuildVersion).MarkAsChanged(username);
+
+    return await service.UpdateProject(buildVersion, cancellationToken);
+  }
+
+  public async Task<BuildVersion?> HandleDelete(string projectName, string username, CancellationToken cancellationToken)
+  {
+    BuildVersion? buildVersion = await service.GetByName(projectName, cancellationToken);
+    if (buildVersion is null)
+    {
+      return null;
+    }
+
+    buildVersion = buildVersion.MarkAsChanged(username);
+
     buildVersion.IsDeleted = true;
 
     return await service.Delete(buildVersion, cancellationToken);
@@ -45,61 +69,4 @@ public class EndpointsService(IStorageService service) : IEndpointsService
   public async Task<BuildVersion?> HandleGetByName(string projectName, CancellationToken cancellationToken) =>
     //HINT Add business logic here: Returns single by ProjectName if not soft deleted
     await service.GetByName(projectName, cancellationToken);
-
-  public async Task<BuildVersion?> HandleIncreaseVersion(string projectName, VersionNumber version, string username, CancellationToken cancellationToken)
-  {
-    //HINT Add business logic here: Increase Version and register the changed and username
-    BuildVersion? buildVersion = await service.GetByName(projectName, cancellationToken);
-    if (buildVersion is null)
-    {
-      return null;
-    }
-
-    switch (version)
-    {
-      case VersionNumber.Major:
-        buildVersion.Major++;
-        buildVersion.Minor = buildVersion.Build = buildVersion.Revision = 0;
-        break;
-
-      case VersionNumber.Minor:
-        buildVersion.Minor++;
-        buildVersion.Build = buildVersion.Revision = 0;
-        break;
-
-      case VersionNumber.Build:
-        buildVersion.Build++;
-        buildVersion.Revision = 0;
-        break;
-
-      case VersionNumber.Revision:
-        buildVersion.Revision++;
-        break;
-    }
-    buildVersion.Changed = DateTime.Now;
-    buildVersion.Username = username;
-
-    return await service.UpdateProject(buildVersion, cancellationToken);
-  }
-
-  public async Task<BuildVersion?> HandleUpdateProject(BuildVersion newBuildVersion, string username, CancellationToken cancellationToken)
-  {
-    //HINT Add business logic here: Update the object and register the changed and username
-    BuildVersion? buildVersion = await service.GetById(newBuildVersion.Id, cancellationToken);
-    if (buildVersion is null)
-    {
-      return null;
-    }
-
-    buildVersion.ProjectName = newBuildVersion.ProjectName;
-    buildVersion.Major = newBuildVersion.Major;
-    buildVersion.Minor = newBuildVersion.Minor;
-    buildVersion.Build = newBuildVersion.Build;
-    buildVersion.Revision = newBuildVersion.Revision;
-    buildVersion.SemanticVersionText = newBuildVersion.SemanticVersionText;
-    buildVersion.Changed = DateTime.Now;
-    buildVersion.Username = username;
-
-    return await service.UpdateProject(buildVersion, cancellationToken);
-  }
 }
