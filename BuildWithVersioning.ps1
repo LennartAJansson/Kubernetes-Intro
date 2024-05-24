28e178cb7ae4588e380f2bb6072498879e6219b3
lennartajansson <lennart.a.jansson@outlook.com>
2024-05-24 16:12:32 +0200

    Frontend

--- BuildWithVersioning.ps1
+++ BuildWithVersioning.ps1
@@ -41,6 +41,6 @@ foreach($name in @(
 	"Version: ${semanticVersion}"
 	"Description: ${description}"
 
-	docker build -f ./${name}/Dockerfile --force-rm -t ${env:REGISTRYHOST}/${lowerName}:${semanticVersion} --build-arg Version="${version}" --build-arg Configuration="${configuration}" --build-arg Description="${description}" .
+	docker build --progress=plain --no-cache -f ./${name}/Dockerfile --force-rm -t ${env:REGISTRYHOST}/${lowerName}:${semanticVersion} --build-arg Version="${version}" --build-arg Configuration="${configuration}" --build-arg Description="${description}" .
 	docker push ${env:REGISTRYHOST}/${lowerName}:${semanticVersion}
 }
